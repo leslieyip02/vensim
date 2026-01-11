e3c4623464e692d0e4cf2ec0a70603ff87bc3545
leslie <leslieyip02@gmail.com>
2026-01-11 23:30:27 +0800

    feat: make inspector views collapsible

--- client/src/views/inspector/EditItemFormView.tsx
+++ client/src/views/inspector/EditItemFormView.tsx
@@ -3,8 +3,13 @@ import { isEdgeId, isNodeId } from "@/models/graph";
 import { EditNodeFormView } from "./EditNodeFormView";
 import { EditEdgeFormView } from "./EditEdgeFormView";
 import { useInteractionStore } from "@/stores/interaction";
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "@/components/ui/collapsible";
+import { Button } from "@/components/ui/button";
+import { LuChevronDown } from "react-icons/lu";
+import { useState } from "react";
 
 export function EditItemFormView() {
+    const [isOpen, setIsOpen] = useState<boolean>(false);
     const { selectedIds } = useInteractionStore((s) => s);
 
     if (selectedIds.length !== 1) {
@@ -14,12 +19,23 @@ export function EditItemFormView() {
     const selectedId = selectedIds[0];
 
     return (
-        <Card className="w-sm z-100 border-b bg-background drop-shadow rounded-md">
-            <CardTitle className="px-6">Edit {isNodeId(selectedId) ? "Node" : "Edge"}</CardTitle>
-            <CardContent>
-                {isNodeId(selectedId) && <EditNodeFormView nodeId={selectedId} />}
-                {isEdgeId(selectedId) && <EditEdgeFormView edgeId={selectedId} />}
-            </CardContent>
+        <Card className="w-sm py-4 z-100 border-b bg-background drop-shadow rounded-md">
+            <Collapsible open={isOpen} onOpenChange={setIsOpen} className="flex flex-col gap-2">
+                <CollapsibleTrigger>
+                    <CardTitle className="px-6 flex flex-row items-center justify-between">
+                        Edit {isNodeId(selectedId) ? "Node" : "Edge"}
+                        <Button variant="ghost" className="bg-transparent">
+                            <LuChevronDown />
+                        </Button>
+                    </CardTitle>
+                </CollapsibleTrigger>
+                <CollapsibleContent>
+                    <CardContent>
+                        {isNodeId(selectedId) && <EditNodeFormView nodeId={selectedId} />}
+                        {isEdgeId(selectedId) && <EditEdgeFormView edgeId={selectedId} />}
+                    </CardContent>
+                </CollapsibleContent>
+            </Collapsible>
         </Card>
     );
 }
--- client/src/views/inspector/TagSelectorView.tsx
+++ client/src/views/inspector/TagSelectorView.tsx
@@ -3,8 +3,12 @@ import { useTagStore } from "@/stores/tag";
 import { TagBadgeView } from "./TagBadgeView";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardTitle } from "@/components/ui/card";
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "@/components/ui/collapsible";
+import { LuChevronDown, LuChevronUp } from "react-icons/lu";
+import { useState } from "react";
 
 export function TagSelectorView() {
+    const [isOpen, setIsOpen] = useState<boolean>(false);
     const { tags, addTag } = useTagStore();
     const { selectedTags, toggleSelectedTag } = useInteractionStore();
 
@@ -13,20 +17,31 @@ export function TagSelectorView() {
     }
 
     return (
-        <Card className="w-sm z-100 border-b bg-background drop-shadow rounded-md">
-            <CardTitle className="px-6">Select Tags</CardTitle>
-            <CardContent className="flex flex-wrap gap-2">
-                {Object.entries(tags).map(([tagId, tag]) => (
-                    <TagBadgeView
-                        isSelected={selectedTags.includes(tagId)}
-                        tag={tag}
-                        onClick={() => toggleSelectedTag(tagId)}
-                    />
-                ))}
-                <Button variant="outline" onClick={() => addTag()}>
-                    + Add tag
-                </Button>
-            </CardContent>
+        <Card className="w-sm py-4 z-100 border-b bg-background drop-shadow rounded-md">
+            <Collapsible open={isOpen} onOpenChange={setIsOpen} className="flex flex-col gap-2">
+                <CollapsibleTrigger>
+                    <CardTitle className="px-6 flex flex-row items-center justify-between">
+                        Select Tags
+                        <Button variant="ghost" className="bg-transparent">
+                            {isOpen ? <LuChevronUp /> : <LuChevronDown />}
+                        </Button>
+                    </CardTitle>
+                </CollapsibleTrigger>
+                <CollapsibleContent>
+                    <CardContent className="flex flex-wrap gap-2">
+                        {Object.entries(tags).map(([tagId, tag]) => (
+                            <TagBadgeView
+                                isSelected={selectedTags.includes(tagId)}
+                                tag={tag}
+                                onClick={() => toggleSelectedTag(tagId)}
+                            />
+                        ))}
+                        <Button variant="outline" onClick={() => addTag()}>
+                            + Add tag
+                        </Button>
+                    </CardContent>
+                </CollapsibleContent>
+            </Collapsible>
         </Card>
     );
 }
