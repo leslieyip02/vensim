1064ece9379d7a0259a5ad96b4967c08b64a07cc
KennyLewi <kennylewi27@gmail.com>
2026-01-31 18:36:32 +0800

    refactor: update toolbar colour and cursor on buttons

--- client/src/components/ui/toggle-group.tsx
+++ client/src/components/ui/toggle-group.tsx
@@ -68,8 +68,8 @@ function ToggleGroupItem({
                     variant: context.variant || variant,
                     size: context.size || size,
                 }),
-                "w-auto min-w-0 shrink-0 px-3 focus:z-10 focus-visible:z-10 m-0.5",
-                "data-[spacing=0]:rounded-none data-[spacing=0]:shadow-none data-[spacing=0]:first:rounded-l-md data-[spacing=0]:last:rounded-r-md data-[spacing=0]:data-[variant=outline]:border-l-0 data-[spacing=0]:data-[variant=outline]:first:border-l",
+                "w-auto min-w-0 shrink-0 px-3 focus:z-10 focus-visible:z-10 m-0.5 cursor-pointer",
+                "data-[spacing=0]:rounded-sm data-[spacing=0]:shadow-none data-[spacing=0]:first:rounded-l-md data-[spacing=0]:last:rounded-r-md data-[spacing=0]:data-[variant=outline]:border-l-0 data-[spacing=0]:data-[variant=outline]:first:border-l",
                 className,
             )}
             {...props}
