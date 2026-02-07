8b364d483b529b62162a9eae311dc3bbc1cd23b6
KennyLewi <kennylewi27@gmail.com>
2026-02-07 17:17:48 +0800

    feat: add custom error handling

--- client/src/actions/room.ts
+++ client/src/actions/room.ts
@@ -6,17 +6,20 @@ const WS_ORIGIN = import.meta.env.VITE_WS_ORIGIN;
 
 export async function createRoom() {
     const state = useGraphStore.getState();
+    try {
+        const res = await fetch(`${API_ORIGIN}/create`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ state }),
+        });
 
-    const res = await fetch(`${API_ORIGIN}/create`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ state }),
-    });
-
-    const { roomId } = await res.json();
-    return roomId;
+        const { roomId } = await res.json();
+        return roomId;
+    } catch {
+        throw new Error("Something went wrong with Room Creation");
+    }
 }
 
 export async function joinRoom(roomId: string) {
-    connectGraphSocket(`${WS_ORIGIN}/join/${roomId}`);
+    await connectGraphSocket(`${WS_ORIGIN}/join/${roomId}`);
 }
