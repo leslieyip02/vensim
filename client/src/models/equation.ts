c8fb3de190375c16c34cd9e9dfa471a9ef9d2817
kellywsq03 <kellywsq03@gmail.com>
2026-02-08 17:30:25 +0800

    Chore: remove unused equation.ts

--- client/src/models/equation.ts
+++
@@ -1,5 +0,0 @@
-type VariableRef = { type: "variable"; id: string };
-type NumberLiteral = { type: "number"; value: number };
-type Operation = { type: "operation"; op: "+" | "-" | "*" | "/"; left: Equation; right: Equation };
-
-export type Equation = VariableRef | NumberLiteral | Operation;
