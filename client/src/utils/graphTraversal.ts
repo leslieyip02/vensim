2eba64a62bd3394c50baf28820f8579c71a0f206
kellywsq03 <kellywsq03@gmail.com>
2026-02-17 16:55:40 +0800

    remove stock from graph traversal

--- client/src/utils/graphTraversal.ts
+++ client/src/utils/graphTraversal.ts
@@ -1,21 +1,11 @@
-import { type Flow, isStockId, type Node, type Stock } from "@/models/graph";
+import { type Flow, isStockId, type Node } from "@/models/graph";
 import { useGraphStore } from "@/stores/graph";
 
-export function getParentEntities(id: string): Array<Node | Flow | Stock> {
+export function getParentEntities(id: string): Array<Node | Flow> {
     const state = useGraphStore.getState();
-    const parentSet: Set<Node | Flow | Stock> = new Set<Node | Flow | Stock>();
+    const parentSet: Set<Node | Flow > = new Set<Node | Flow >();
 
     const thisEntity = state.nodes[id] ?? state.flows[id] ?? state.stocks[id];
-
-    if (isStockId(id)) {
-        const incomingFlows = Object.values(state.flows).filter((flow) => flow.to === id);
-        // Not a parent but referenced in equation
-        const outgoingFlows = Object.values(state.flows).filter((flow) => flow.from === id);
-        incomingFlows.forEach((flow) => parentSet.add(flow));
-        outgoingFlows.forEach((flow) => parentSet.add(flow));
-        parentSet.add(thisEntity);
-        return [...parentSet];
-    }
 
     const incomingEdges = Object.values(state.edges).filter((edge) => edge.to === id);
     for (const edge of incomingEdges) {
