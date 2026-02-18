dcd884390c47ada160b56051e749f6e404b782cd
KennyLewi <kennylewi27@gmail.com>
2026-02-18 12:40:11 +0800

    chore: lint

--- client/src/views/App.tsx
+++ client/src/views/App.tsx
@@ -9,7 +9,6 @@ import { ErrorModalView } from "./error/ErrorModalView";
 import { HelpModalView } from "./help/HelpModalView";
 import { InspectorPanelView } from "./inspector/InspectorPanelView";
 import { RoomModalView } from "./room/RoomModalView";
-import { SimulationModalView } from "./simulate/SimulationModalView";
 import { ToolbarView } from "./toolbar/ToolbarView";
 
 function App() {
