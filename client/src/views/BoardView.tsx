9dc9d93dbfdb05edf33fbffeb295e7da9bd72298
leslie <leslieyip02@gmail.com>
2026-02-22 17:36:02 +0800

    refactor: move graph views

--- client/src/views/BoardView.tsx
+++ client/src/views/BoardView.tsx
@@ -5,21 +5,10 @@ import { useCameraController } from "@/controllers/camera";
 import { useInteractionController as useInteractionController } from "@/controllers/interaction";
 import { useKeyboardShortcuts } from "@/controllers/keyboard";
 
-import { useGraphStore } from "../stores/graph";
-import { CloudView } from "./graph/CloudView";
-import { EdgeView } from "./graph/EdgeView";
-import { FlowView } from "./graph/FlowView";
-import { NodeView } from "./graph/NodeView";
-import { StockView } from "./graph/StockView";
+import { GraphView } from "./graph/GraphView";
 import { GridView } from "./GridView";
 
 export function BoardView() {
-    const nodes = Object.values(useGraphStore((s) => s.nodes));
-    const edges = Object.values(useGraphStore((s) => s.edges));
-    const stocks = Object.values(useGraphStore((s) => s.stocks));
-    const clouds = Object.values(useGraphStore((s) => s.clouds));
-    const flows = Object.values(useGraphStore((s) => s.flows));
-
     const cameraController = useCameraController({
         x: window.innerWidth / 2,
         y: window.innerHeight / 2,
@@ -53,21 +42,7 @@ export function BoardView() {
         <Stage {...stageProps}>
             <Layer>
                 <GridView />
-                {nodes.map((node) => (
-                    <NodeView key={node.id} node={node} />
-                ))}
-                {edges.map((edge) => (
-                    <EdgeView key={edge.id} edge={edge} />
-                ))}
-                {stocks.map((stock) => (
-                    <StockView key={stock.id} stock={stock} />
-                ))}
-                {clouds.map((cloud) => (
-                    <CloudView key={cloud.id} cloud={cloud} />
-                ))}
-                {flows.map((flow) => (
-                    <FlowView key={flow.id} flow={flow} />
-                ))}
+                <GraphView />
             </Layer>
         </Stage>
     );
