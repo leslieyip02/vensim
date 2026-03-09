ebe5310584c5cbf578f4252aa24ff68aa1c75ae8
KennyLewi <kennylewi27@gmail.com>
2026-03-09 22:38:23 +0800

    chore: mark todo for simulation route validation in backend

--- server/sim/routes.go
+++ server/sim/routes.go
@@ -13,6 +13,8 @@ func RunSimulation(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// todo
+
 	res, err := simulate(req)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
